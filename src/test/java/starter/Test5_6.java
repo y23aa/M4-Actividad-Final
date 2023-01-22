e5ac5c3015624a3a2aba50918bdc62f1dbd60a90
Jose Serra <serrajv@gmail.com>
2023-01-22 01:34:52 +0100

    Test6 implemented, small refactor for the locators

--- src/test/java/starter/Test5_6.java
+++ src/test/java/starter/Test5_6.java
@@ -23,11 +23,15 @@ import static org.openqa.selenium.support.ui.ExpectedConditions.alertIsPresent;
 @RunWith(SerenityRunner.class)
 public class Test5_6 {
     private static final String WEBSITE = "https://www.demoblaze.com/index.html";
-    private static final String PHONES_LOCATOR = "//a[@onclick=\"byCat('phone')\"]";
-    private static final String PHONE_S6_LOCATOR = "//a[@href=\"prod.html?idp_=1\"]";
-    private static final String ADD_TO_CART_LOCATOR = "//a[@onclick=\"addToCart(1)\"]";
-    private static final String VIEW_CART_LOCATOR = "cartur";
-    private static final String CART_FIRST_ROW_NAME_LOCATOR = "//*[@id='tbodyid']/tr/td[2]";
+    private static final By PHONES_LOCATOR = By.xpath("//a[@onclick=\"byCat('phone')\"]");
+    private static final By LAPTOPS_LOCATOR = By.xpath("//a[@onclick=\"byCat('notebook')\"]");
+    private static final By PHONE_S6_LOCATOR = By.xpath("//a[@href=\"prod.html?idp_=1\"]"); // ToDo: create locators from ids numbers
+    private static final By ADD_TO_CART_PHONE_S6_LOCATOR = By.xpath("//a[@onclick=\"addToCart(1)\"]"); // ToDo: create locators from ids numbers
+    private static final By LAPTOP_VAIO_LOCATOR = By.xpath("//a[@href=\"prod.html?idp_=9\"]"); // ToDo: create locators from ids numbers
+    private static final By ADD_TO_CART_LAPTOP_VAIO_LOCATOR = By.xpath("//a[@onclick=\"addToCart(9)\"]"); // ToDo: create locators from ids numbers
+    private static final By VIEW_CART_LOCATOR = By.id("cartur");
+    private static final By CART_FIRST_ROW_NAME_LOCATOR = By.xpath("//*[@id='tbodyid']/tr/td[2]");
+    private static final By PLACE_ORDER_LOCATOR = By.xpath("//button[@data-target='#orderModal']");
     @Managed
     WebDriver driver;
     WebDriverWait wait;
@@ -35,21 +39,34 @@ public class Test5_6 {
     @Before
     public void setUp(){
         wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        driver.get(WEBSITE);
     }
 
     @Test
-    public void remove_s6_from_cart(){
-        driver.get(WEBSITE);
-        driver.findElement(By.xpath(PHONES_LOCATOR)).click();
-        driver.findElement(By.xpath(PHONE_S6_LOCATOR)).click();
-        WebElement addToCartButton = driver.findElement(By.xpath(ADD_TO_CART_LOCATOR));
+    public void test5_remove_s6_from_cart(){
+        driver.findElement(PHONES_LOCATOR).click();
+        driver.findElement(PHONE_S6_LOCATOR).click();
+        WebElement addToCartButton = driver.findElement(ADD_TO_CART_PHONE_S6_LOCATOR);
         wait.until(elementToBeClickable(addToCartButton));
         addToCartButton.click();
         wait.until(alertIsPresent());
         driver.switchTo().alert().accept();
-        driver.findElement(By.id(VIEW_CART_LOCATOR)).click();
-        String title = driver.findElement(By.xpath(CART_FIRST_ROW_NAME_LOCATOR)).getText();
+        driver.findElement(VIEW_CART_LOCATOR).click();
+        String title = driver.findElement(CART_FIRST_ROW_NAME_LOCATOR).getText();
         assertThat(title).isEqualToIgnoringCase("Samsung galaxy s6");
+    }
+
+    @Test
+    public void test6_do_checkout(){
+        driver.findElement(LAPTOPS_LOCATOR).click();
+        driver.findElement(LAPTOP_VAIO_LOCATOR).click();
+        WebElement addToCartButton2 = driver.findElement(ADD_TO_CART_LAPTOP_VAIO_LOCATOR);
+        wait.until(elementToBeClickable(addToCartButton2));
+        addToCartButton2.click();
+        wait.until(alertIsPresent());
+        driver.switchTo().alert().accept();
+        driver.findElement(VIEW_CART_LOCATOR).click();
+        driver.findElement(PLACE_ORDER_LOCATOR).click();
     }
 
 
