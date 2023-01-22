ac57985aee3b0cfd8a59adde8f7cc294bb246ec4
Admin <julloamo42@alumnes.ub.edu>
2023-01-22 17:39:54 -0600

    Actualizacion del Test 1 y 2.

--- src/test/java/starter/Test1_2.java
+++ src/test/java/starter/Test1_2.java
@@ -2,90 +2,70 @@ package starter;
 
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Managed;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.Alert;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.When;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.After;
-import static org.junit.Assert.assertEquals;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
+import static org.openqa.selenium.support.ui.ExpectedConditions.alertIsPresent;
 
 @RunWith(SerenityRunner.class)
 public class Test1_2 {
+    private static final String WEBSITE = "https://www.demoblaze.com/index.html";
+    private static final By SIGNUP_LOCATOR = By.xpath("//*[@id=\"signin2\"]");
+    private static final By USERNAME_LOCATOR = By.xpath("//*[@id=\"sign-username\"]");
 
     @Managed
     WebDriver driver;
-    private String username;
-    private String password;
+    WebDriverWait wait;
 
-    @Given("I am on the website")
-    public void i_am_on_the_website() {
-        driver = new ChromeDriver();
-        driver.get("https://www.demoblaze.com");
+    @Before
+    public void setUp() {
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        driver.get(WEBSITE);
     }
 
-    @When("I click on the signup button")
-    public void i_click_on_the_signup_button() {
-        WebElement signupButton = driver.findElement(By.id("signin2"));
-        signupButton.click();
+    @Test
+    public void test1_signup() {
+        WebElement SignupButton = driver.findElement(SIGNUP_LOCATOR);
+        wait.until(elementToBeClickable(SignupButton));
+        SignupButton.click();
+        driver.findElement(USERNAME_LOCATOR).click();
+        driver.findElement(By.cssSelector("#signin2")).click();
+        driver.findElement(By.name("Username")).click();
+        driver.findElement(By.name("Username")).sendKeys("UserSiglo12");
+        driver.findElement(By.name("Password")).click();
+        driver.findElement(By.name("Password")).sendKeys("passw123456");
+        driver.findElement(By.name("Sign up")).click();
+        driver.findElement(By.name("Ok")).click();
+        driver.switchTo().alert().accept();
     }
 
-    @When("I fill in {string} as the username")
-    public void i_fill_in_as_the_username(String username) {
-        WebElement usernameField = driver.findElement(By.id("sign-username"));
-        usernameField.sendKeys("DTXaviH");
-        this.username = "DTXaviH";
-    }
-
-    @When("I fill in {string} as the password")
-    public void i_fill_in_as_the_password(String password) {
-        WebElement passwordField = driver.findElement(By.id("sign-password"));
-        passwordField.sendKeys("passw123456");
-        this.password = "passw123456";
-    }
-
-    @When("I click on the signup button 2")
-    public void i_click_on_the_signup_button_2() {
-        WebElement submitButton = driver.findElement(By.className("btn btn-primary"));
-        submitButton.click();
-        Alert alert = driver.switchTo().alert();
-        alert.accept();
-    }
-
-    @When("I click on the login button")
-    public void i_click_on_the_login_button() {
-        WebElement loginButton = driver.findElement(By.id("login2"));
-        loginButton.click();
-    }
-
-    @When("I fill in the same username as before")
-    public void i_fill_in_the_same_username_as_before() {
-        WebElement usernameField = driver.findElement(By.id("loginusername"));
-        usernameField.sendKeys(this.username);
-    }
-
-    @When("I fill in the same password as before")
-    public void i_fill_in_the_same_password_as_before() {
-        WebElement passwordField = driver.findElement(By.id("loginpassword"));
-        passwordField.sendKeys(this.password);
-    }
-
-    @When("I click on the login button 2")
-    public void i_click_on_the_login_button_2() {
-        WebElement loginSubmitButton = driver.findElement(By.className("btn btn-primary"));
-        loginSubmitButton.click();
-    }
-
-    @Then("I should be logged in")
-    public void i_should_be_logged_in() {
-        String expectedUrl = "https://www.demoblaze.com";
-        String actualUrl = driver.getCurrentUrl();
-        assertEquals(expectedUrl, actualUrl);
-    }
+//    @Test
+//    public void test2_login() {
+//        driver.get("https://www.demoblaze.com/index.html");
+//        driver.findElement(By.cssSelector("#login2")).click();
+//        driver.findElement(By.name("Username")).click();
+//        driver.findElement(By.name("Username")).sendKeys("UserSiglo12");
+//        driver.findElement(By.name("Password")).click();
+//        driver.findElement(By.name("Password")).sendKeys("passw123456");
+//        driver.findElement(By.name("Log in")).click();
+//        driver.findElement(By.name("Ok")).click();
+//        String expectedUrl = "https://www.demoblaze.com/index.html";
+//        String actualUrl = driver.getCurrentUrl();
+//        assertThat(actualUrl).isEqualTo(expectedUrl);
+//    }
 
     @After
     public void close_browser() {
