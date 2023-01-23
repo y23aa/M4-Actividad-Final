7e2c10c11aa58fb5f5098762fcc604cfdfd86452
Jose Serra <serrajv@gmail.com>
2023-01-23 04:19:27 +0100

    Tests run in a predefined order.

--- src/test/java/starter/Test1_2.java
+++ src/test/java/starter/Test1_2.java
@@ -5,7 +5,9 @@ import net.thucydides.core.annotations.Managed;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.FixMethodOrder;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -19,6 +21,7 @@ import static org.openqa.selenium.support.ui.ExpectedConditions.alertIsPresent;
 import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
 
 @RunWith(SerenityRunner.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class Test1_2 {
     private static final String WEBSITE = "https://www.demoblaze.com/index.html";
     private static final By SIGNUP_LOCATOR = By.id("signin2");
@@ -32,8 +35,8 @@ public class Test1_2 {
     private static final By PASSWORD2_LOCATOR = By.id("loginpassword");
     private static final By LOGGED_USER_LOCATOR = By.id("nameofuser");
 
-    private static final String TEST_NAME="start121sdf233";
-    private static final String TEST_PWD="marte45sdf123";
+    private static final String TEST_NAME="start121sdf23s";
+    private static final String TEST_PWD="marte45sdf123s";
 
     private void sleepmSecs(int mSec){
         try {
@@ -60,8 +63,8 @@ public class Test1_2 {
         WebElement SignupButton = driver.findElement(SIGNUP_LOCATOR);
         wait.until(elementToBeClickable(SignupButton));
         SignupButton.click();
-        driver.findElement(USERNAME_LOCATOR).sendKeys("start121sdf233");
-        driver.findElement(PASSWORD_LOCATOR).sendKeys("marte45sdf123");
+        driver.findElement(USERNAME_LOCATOR).sendKeys(TEST_NAME);
+        driver.findElement(PASSWORD_LOCATOR).sendKeys(TEST_PWD);
         driver.findElement(SIGNUP_2_LOCATOR).click();
         wait.until(alertIsPresent());
         String alertText=driver.switchTo().alert().getText();
@@ -82,7 +85,6 @@ public class Test1_2 {
         wait.until(presenceOfElementLocated(LOGGED_USER_LOCATOR));
         String loggedUserName = driver.findElement(LOGGED_USER_LOCATOR).getText();
         assertThat(loggedUserName).isEqualToIgnoringCase("Welcome " + TEST_NAME);
-
     }
 
     @After
